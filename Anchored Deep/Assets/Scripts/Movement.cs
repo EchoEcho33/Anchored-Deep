772db63d128790d152d0ffba8b23e5f2195dc08e
Sophia Regnault <90007008+EchoEcho33@users.noreply.github.com>
2025-03-12 15:43:48 -0400

    AAAAAAAAAAAA

--- Anchored Deep/Assets/Scripts/Movement.cs
+++ Anchored Deep/Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,27 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private CharacterController controller;
+   private Vector3 playerVelocity;
+   private bool groundedPlayer;
+   [SerializeField]
+   private float playerSpeed = 10.0f;
+   private float jumpHeight = 1.0f;
+   private float gravityValue = -9.81f;
 
-    // Update is called once per frame
+    private void Start()
+   {
+       controller = gameObject.GetComponent<CharacterController>();
+   }
+
     void Update()
-    {
-        
-    }
+   {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+   }
 }
