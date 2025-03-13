4724ef792c899da9fbc91a517f55203c53fd86dd
Sophia Regnault <90007008+EchoEcho33@users.noreply.github.com>
2025-03-13 12:39:08 -0400

    THE BOAT MOVES!!!
    :D

--- Anchored Deep/Assets/Scripts/Movement.cs
+++ Anchored Deep/Assets/Scripts/Movement.cs
@@ -2,17 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
    private CharacterController controller;
    private Vector3 playerVelocity;
-   private bool groundedPlayer;
    [SerializeField]
-   private float playerSpeed = 10.0f;
-   private float jumpHeight = 1.0f;
-   private float gravityValue = -9.81f;
+   private float playerSpeed = 1.0f;
+   [SerializeField]
+   private float rotationSpeed = 20.0f;
 
     private void Start()
    {
@@ -21,12 +21,21 @@ public class Movement : MonoBehaviour
 
     void Update()
    {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
+        if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0), Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(new Vector3(0, -rotationSpeed * Time.deltaTime, 0), Space.Self);
+        }
+        if (Input.GetKey(KeyCode.W)){
+            Vector3 parentAngle = transform.eulerAngles;
+            Vector3 rotated = Quaternion.Euler(parentAngle) * new Vector3(0, 0, 1);
+            transform.position += new Vector3(rotated.x, 0, rotated.z) * playerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            Vector3 parentAngle = transform.eulerAngles;
+            Vector3 rotated = Quaternion.Euler(parentAngle) * new Vector3(0, 0, -1);
+            transform.position += new Vector3(rotated.x, 0, rotated.z) * playerSpeed * Time.deltaTime;
         }
    }
 }
