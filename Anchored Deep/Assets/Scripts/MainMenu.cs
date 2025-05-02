ab3f3717ae047e54f2fae49a947848f836b2e266
tcantu-g <tpc3@gatech.edu>
2025-05-02 03:57:31 -0400

    RAHHH (UI)

--- Anchored Deep/Assets/Scripts/MainMenu.cs
+++ Anchored Deep/Assets/Scripts/MainMenu.cs
@@ -11,5 +11,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Ocean");
     }
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
     
 }
---
+++ Anchored Deep/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1f; // Ensure game starts running
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape key pressed!");
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Title");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
